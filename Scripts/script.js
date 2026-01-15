429cb014c6e656b73d7ff7271513a3810244b559
Steve <peersalexandr@gmail.com>
2026-01-15 13:51:51 +0500

    separation is done

--- Scripts/script.js
+++ Scripts/script.js
@@ -38,3 +38,25 @@ window.addEventListener('scroll', () => {
     step.classList.toggle('active', index === activeIndex);
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
